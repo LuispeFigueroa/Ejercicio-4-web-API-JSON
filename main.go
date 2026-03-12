1363f92b89a305f0f4c4d2bdf17107232908b3b3
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-03-12 17:10:22 -0600

    agregar patch y ajustar handles

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func handleGetFighter(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	idParam := query.Get("id")
 
-	if idParam != "" {
+	if idParam == "" {
 		writeJSON(w, http.StatusOK, fighters)
 		return
 	}
@@ -107,7 +107,7 @@ func handlePostFighter(w http.ResponseWriter, r *http.Request) {
 	//se genera un nuevo ID para el nuevo luchador y se agrega a la lista de luchadores
 	newFighter.ID = generateNextID()
 	fighters = append(fighters, newFighter)
-	saveTeams()
+	saveFighters()
 
 	writeJSON(w, http.StatusCreated, newFighter)
 }
@@ -133,11 +133,66 @@ func saveFighters() {
 		return
 	}
 
-	err = os.WriteFile("data/teams.json", data, 0644)
+	err = os.WriteFile("data/fighters.json", data, 0644)
 	if err != nil {
 		log.Println("Error writing JSON file:", err)
 	}
+}
 
+func handlePatchFighter(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	idParam := query.Get("id")
+	//si no se proporciona un ID, se devuelve un error indicando que el ID es requerido
+	if idParam == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+	//se comprueba que la ID sea un numero valido y que exista en la lista de luchadores, si no se encuentra se devuelve un error indicando que el ID es invalido
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+	//se decodifica el cuerpo de la solicitud para obtener los campos que se desean actualizar, si el cuerpo de la solicitud no es un JSON valido se devuelve un error indicando que el cuerpo de la solicitud es invalido
+	var updates map[string]interface{}
+	err = json.NewDecoder(r.Body).Decode(&updates)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	// se recorren todos los luchadores hasta encontrar el luchador con la ID proporcionada,
+	// si se encuentra se actualizan los campos que se desean actualizar y se guarda la lista de luchadores en el archivo JSON,
+	// si no se encuentra se devuelve un error indicando que el luchador no fue encontrado
+	for i, fighter := range fighters {
+		if fighter.ID == id {
+
+			if name, ok := updates["name"].(string); ok {
+				fighters[i].Name = name
+			}
+
+			if country, ok := updates["country"].(string); ok {
+				fighters[i].Country = country
+			}
+
+			if record, ok := updates["record"].(string); ok {
+				fighters[i].Record = record
+			}
+
+			if specialty, ok := updates["specialty"].(string); ok {
+				fighters[i].Specialty = specialty
+
+			}
+
+			if height, ok := updates["height"].(string); ok {
+				fighters[i].Height = height
+			}
+
+			saveFighters()
+			writeJSON(w, http.StatusOK, fighters[i])
+			return
+		}
+	}
+	http.Error(w, "Fighter not found", http.StatusNotFound)
 }
 
 func writeJSON(w http.ResponseWriter, status int, payload interface{}) {
